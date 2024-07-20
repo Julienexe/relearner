247ef31ba17c5de8788fca4cdd61e819a468394c
Julianexe <julianssonko@gmail.com>
2024-07-21 01:16:43 +0300

    feat: Add package page for displaying package details and selecting modules

---
+++ lib/pages/package_page.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_multi_select_items/flutter_multi_select_items.dart';
+import 'package:multi_select_flutter/util/multi_select_item.dart';
+import 'package:relearner/main.dart';
+import 'package:relearner/modules/general_modules.dart';
+
+class PackagePage extends StatelessWidget {
+  const PackagePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('reLearner', style: bannerTextStyle),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: SizedBox(
+          height: 700,
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Single Module', style: bannerTextStyle),
+                    const Spacer(),
+                    const Text('UGX', style: defaultTextStyle),
+                    const SizedBox(
+                      width: 4,
+                    ),
+                    Text(
+                      '100000',
+                      style: bannerTextStyle,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  'For chronological consumption of modules in a predetermined order',
+                  style: defaultTextStyle.copyWith(fontSize: 20),
+                ),
+                const SizedBox(height: 16),
+                Text('Select a module to learn', style: bannerTextStyle),
+                const SizedBox(height: 8),
+                MultiSelectContainer(
+                  showInListView: false,
+                    itemsDecoration: MultiSelectDecorations(
+                      decoration: BoxDecoration(
+                        border:
+                            Border.all(color: themeColor.withOpacity(0.5)),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    textStyles:  MultiSelectTextStyles(
+                        textStyle: bannerTextStyle.copyWith(fontSize: 17)),
+                    items: [
+                      ...getCourses(),
+                      
+                    ],
+                    onChange: (allSelectedItems, selectedItem) {}),
+                const SizedBox(height: 16),
+                Text('Start Learning Today', style: bannerTextStyle),
+                const SizedBox(height: 8),
+                const FeatureListItem(text: 'Self-Paced Learning Option'),
+                const FeatureListItem(text: 'Practice Quizzes'),
+                const FeatureListItem(text: 'Course videos and readings'),
+                const FeatureListItem(text: 'Downloadable resources'),
+                const SizedBox(height: 24),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 32, vertical: 16),
+                      foregroundColor: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
+                    child: Text(
+                      'ENROLL FOR THIS PACKAGE',
+                      style: bannerTextStyle.copyWith(fontSize: 18),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FeatureListItem extends StatelessWidget {
+  final String text;
+
+  const FeatureListItem({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Row(
+        children: [
+          const Icon(Icons.check, color: themeColor),
+          const SizedBox(width: 8),
+          Expanded(
+              child:
+                  Text(text, style: defaultTextStyle.copyWith(fontSize: 20))),
+        ],
+      ),
+    );
+  }
+}
+
+List<MultiSelectCard> getCourses(){
+  List<MultiSelectCard> courses = [];
+  
+  if (appState.courses != null) {
+    for (var course in appState.courses!) {
+      courses.add(MultiSelectCard(
+        value: course.name,
+        label: course.name,
+      ));
+    }
+    
+  }
+  return courses;
+}
